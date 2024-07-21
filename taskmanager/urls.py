aae868be6f88a42291b65301abaacfb591d47f5b
mbharath246 <bharathroyal246@gmail.com>
2024-07-21 23:13:03 +0530

    added static urls

--- taskmanager/urls.py
+++ taskmanager/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -53,3 +54,5 @@ urlpatterns = [
     path('token/verify/',TokenVerifyView.as_view(), name='token-verify'),
 
 ]
+
+urlpatterns += staticfiles_urlpatterns
