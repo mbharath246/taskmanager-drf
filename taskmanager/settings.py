90f75740eea45b76868b971ab8db96d83134ded3
mbharath246 <bharathroyal246@gmail.com>
2024-07-22 14:34:12 +0530

    added whitenoise for static

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -136,7 +136,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-# STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = 'staticfiles'
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
--- taskmanager/urls.py
+++ taskmanager/urls.py
@@ -14,13 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -55,9 +53,3 @@ urlpatterns = [
     path('token/verify/',TokenVerifyView.as_view(), name='token-verify'),
 
 ]
-
-# urlpatterns += staticfiles_urlpatterns()
-
-# if settings.DEBUG:
-#     from django.conf.urls.static import static
-#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
